efcfe07f065bfbf0fa83fc315e22147600edbcec
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2021-07-19 15:14:12 +0200

    Add comment

--- jupyterhub-gcp-deployment/resize-kubernetes.sh
+++ jupyterhub-gcp-deployment/resize-kubernetes.sh
@@ -1,4 +1,6 @@
 
+# Note, bringing down the JupyterHub Kubernetes cluster is more complex than scaling down things like this!
+
 # https://cloud.google.com/kubernetes-engine/docs/how-to/managing-clusters#resizing_clusters
 # https://cloud.google.com/kubernetes-engine/docs/how-to/resizing-a-cluster
 
