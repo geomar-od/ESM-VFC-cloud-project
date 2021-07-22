0d7aa61386be050e971c77ac4dd2af3ed275b5a9
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2021-07-22 18:15:33 +0200

    Adapt default user scheduling (#10)
    * Rename after purpose
    
    * Specify current development setup

--- jupyterhub-gcp-deployment/gke-cluster-setup.sh
+++ jupyterhub-gcp-deployment/gke-cluster-setup.sh
@@ -1,13 +1,15 @@
 
 # https://zero-to-jupyterhub.readthedocs.io/en/latest/kubernetes/google/step-zero-gcp.html
 
+source gcp-config.sh
+
 # gcloud compute machine-types list | head -n 1
-# gcloud compute machine-types list | grep europe-west3
+# gcloud compute machine-types list | grep europe-west3 # Frankfurt
 
 gcloud container clusters create \
-  --zone europe-west3-b \
+  --zone ${GCP_RESOURCE_ZONE} \
   --machine-type e2-standard-2 \
-  --num-nodes 2 \
+  --num-nodes 1 \
   --cluster-version latest \
-  jupyterhub-cluster-1
+  ${GKE_CLUSTER}
 
