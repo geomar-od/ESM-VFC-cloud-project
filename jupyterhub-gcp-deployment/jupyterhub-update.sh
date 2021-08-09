03b538f61e57f21e6345205c8b681a0fcab5493c
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2021-08-09 19:35:50 +0200

    Add workload identity deploy scripts

--- jupyterhub-gcp-deployment/jupyterhub-update.sh
+++ jupyterhub-gcp-deployment/jupyterhub-update.sh
@@ -7,7 +7,7 @@ helm upgrade \
   ${HELM_RELEASE} jupyterhub/jupyterhub \
   --namespace ${K8S_NAMESPACE} \
   --cleanup-on-fail \
-  --version=1.0.1 \
+  --version=1.1.1 \
   --values config.yaml
 
 kubectl get pod --namespace ${K8S_NAMESPACE}
