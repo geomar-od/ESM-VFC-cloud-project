9a0eaebce00966c1f6a63da2b1843d0b56d355e9
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2022-01-29 22:45:57 +0100

    Add forgotten config

--- dask-gateway-deploy/config.sh
+++ dask-gateway-deploy/config.sh
@@ -8,24 +8,37 @@ GCP_RESOURCE_ZONE=${GCP_RESOURCE_ZONE}
 
 # Dask gateway default node pool.
 
-GKE_CLUSTER_NAME=dask-gateway-1
+GKE_CLUSTER_NAME=daskgateway2
 
 # Kubernetes namespace.
 
-K8S_NAMESPACE=dask-gateway-1
+K8S_NAMESPACE=daskgateway2
 
 # Helm release options.
 
-HELM_RELEASE_NAME=dask-gateway-1
+HELM_RELEASE_NAME=daskgateway2
 HELM_CHART_VERSION=0.9.0
 
 # Access permission options.
-# These should be arrays?
 
-IAM_SERVICE_ACCOUNT_NAME_PREFIX=${IAM_SERVICE_ACCOUNT_NAME_PREFIX}
-IAM_SERVICE_ACCOUNT_NAME=${IAM_SERVICE_ACCOUNT_NAME_PREFIX}@${GCP_BILLING_PROJECT}.iam.gserviceaccount.com
+IAM_SERVICE_ACCOUNT_PREFIX=daskgateway2
 
-# Deploy Gateway in JupyterHub namespace.
+IAM_SERVICE_ACCOUNT_SUFFIX=${GCP_BILLING_PROJECT}.iam.gserviceaccount.com # Don't change.
+IAM_SERVICE_ACCOUNT_NAME=${IAM_SERVICE_ACCOUNT_PREFIX}@${IAM_SERVICE_ACCOUNT_SUFFIX} # Don't change.
+GKE_WORKLOAD_POOL=${GCP_BILLING_PROJECT}.svc.id.goog # Don't change.
 
-GKE_CLUSTER_NAME=jupyterhub-cluster-1
-K8S_NAMESPACE=jupyterhub-k8s-1
+# Deploy Gateway alongside JupyterHub.
+# If you’re not deploying Dask-Gateway in the same cluster and namespace as JupyterHub, you will need to specify JupyterHub’s API url.
+# If JupyterHub and Dask-Gateway are on the same cluster and namespace you can omit this configuration key, and the address will be inferred automatically.
+# https://gateway.dask.org/install-kube.html#authenticating-with-jupyterhub
+# This is convenient, therefore specify the JupyterHub's Kubernetes namespace.
+
+GKE_CLUSTER_NAME=jupyterhub2
+K8S_NAMESPACE=jupyterhub2
+
+# For simplicity, we'll also rely on JupyterHub's service account for non-public storage authorization here.
+
+IAM_SERVICE_ACCOUNT_PREFIX=jupyterhub2
+IAM_SERVICE_ACCOUNT_SUFFIX=${GCP_BILLING_PROJECT}.iam.gserviceaccount.com # Don't change.
+IAM_SERVICE_ACCOUNT_NAME=${IAM_SERVICE_ACCOUNT_PREFIX}@${IAM_SERVICE_ACCOUNT_SUFFIX} # Don't change.
+GKE_WORKLOAD_POOL=${GCP_BILLING_PROJECT}.svc.id.goog # Don't change.
