0188fab1ac38019fca3a88202ae91e28082c04a0
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2022-02-05 21:41:11 +0100

    Extend NESH experiment

--- performance/dask-worker.sh
+++ performance/dask-worker.sh
@@ -4,8 +4,8 @@
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1 --ntasks-per-node=1
 #SBATCH --job-name=daskworker
-###SBATCH --constraint=cascade,mem192 # NESH
-#SBATCH --constraint=skylake,mem192 # NESH
+#SBATCH --constraint=cascade,mem192 # NESH
+###SBATCH --constraint=skylake,mem192 # NESH
 
 module load singularity/3.6.4
 
@@ -13,4 +13,4 @@ singularity run \
 --bind /gxfs_work1/geomar/smomw260/github/ESM-VFC-cloud-project \
 --bind /scratch \
 /gxfs_home/geomar/smomw260/github/ESM-VFC-cloud-project/performance/pangeo-notebook_2021.07.17.sif \
-dask-worker 172.18.4.12:46517 --interface ib0 --nthreads 7 --memory-limit 32GiB
+dask-worker 172.18.4.12:34981 --interface ib0 --nthreads 7 --memory-limit 32GiB
